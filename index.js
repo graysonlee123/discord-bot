941bc6c3b1bd9568ef4a19a0719ff6fdb041705c
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-12 12:48:44 -0400

    adds args, command variables; easier IF statements

--- index.js
+++ index.js
@@ -11,23 +11,21 @@ client.on("message", message => {
   // If the message doesn't have a prefix, or if it is from the bot itself, return
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // Grabs the arguments after the space in the arguments
+  // Adds a space after the command prefix
   const args = message.content.slice(prefix.length).split(" ");
   // shift() will take the first element in array and return it while also removing it from the original array
   const command = args.shift().toLowerCase();
 
-  if (message.content.startsWith(`${prefix}join`)) {
-    message.channel.send("I got the join request! Epic.");
-  } else if (message.content.startsWith(`${prefix}leave`)) {
-    message.channel.send("I got the leave request... bummer...");
-  } else if (message.content === `${prefix}server`) {
-    message.channel.send(`This server's name is: ${message.guild.name}
-    Total members: ${message.guild.memberCount}
-    Created: ${message.guild.createdAt}
-    Owner: ${message.guild.owner}`);
-  } else if (message.content === `${prefix}user-info`) {
-    message.channel.send(`Your username: ${message.author.username}
-    Your ID: ${message.author.id}`);
+  if (command === "args-info") {
+    if (!args.length) {
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
+    } else if (args[0] === "foo") {
+      return message.channel.send("bar");
+    }
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 });
 
