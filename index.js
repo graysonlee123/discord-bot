f64bb319b633e06d0e6d2534000df78747821b48
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-15 15:01:14 -0400

    adds better way of managing commands; aliases; args; help command

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-const { token, prefix } = require("./config.json");
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-const Discord = require("discord.js");
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-client.once("ready", () => {
-  console.log("Ready!");
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'));
+  
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+  console.log('Ready!');
 });
 
-client.on("message", message => {
-  // If the message doesn't have a prefix, or if it is from the bot itself, return
+client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // Adds a space after the command prefix
-  const args = message.content.slice(prefix.length).split(" ");
-  // shift() will take the first element in array and return it while also removing it from the original array
-  const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-  if (command === "args-info") {
-    if (!args.length) {
-      return message.channel.send(
-        `You didn't provide any arguments, ${message.author}!`
-      );
-    } else if (args[0] === "foo") {
-      return message.channel.send("bar");
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!111!!1!!!11')
+  }
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \'${prefix}${command.name} ${command.usage}\'`
     }
+    
+    return message.channel.send(reply)
+  }
 
-    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \'${command.name}\' command.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
   }
 });
 
